02dc12fbbbfff9966a721f29324dd54aa432365c
bitnami <ej48@duke.edu>
2015-02-14 20:26:08 +0000

    group model edit

--- groups/models.py
+++ groups/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+from authentication.models import Account
+
+class KGroup(models.Model):
+    name = models.CharField(max_length=50)
+    owner = models.ForeignKey(Account)
+    members = models.ManyToManyField(Account)    
+
+    def __unicode__(self):
+        return '{0}'.format(self.name)
