92f713338e0326000e3c515ec0b826d16b40b6c2
Benson <benson.tran@duke.edu>
2015-02-14 17:33:30 -0500

    edited jumbotron

--- static/javascripts/accounts/controllers/account.controller.js
+++ static/javascripts/accounts/controllers/account.controller.js
@@ -32,11 +32,31 @@
          * @memberOf kalendr.accounts.controllers.AccountController
          */
         function activate() {
+
             var username = $routeParams.username.substr(1);
 
             Account.get(username).then(accountSuccessFn, accountErrorFn);
             Posts.getWeek(username, null).then(postsSuccessFn, postsErrorFn);
             Authentication.getUsers().then(usersSuccessFn);
+
+            var now = new Date();
+            var dayOfWeek;
+
+            var num_day = now.getDay();
+
+            if (num_day == 0) dayOfWeek = 'Sunday';
+            else if (num_day == 1) dayOfWeek = 'Monday';
+            else if (num_day == 2) dayOfWeek = 'Tuesday';
+            else if (num_day == 3) dayOfWeek = 'Wednesday';
+            else if (num_day == 4) dayOfWeek = 'Thursday';
+            else if (num_day == 5) dayOfWeek = 'Friday';
+            else dayOfWeek = 'Saturday';
+
+
+
+            var date = Date().substring(3, 10);
+            vm.date = dayOfWeek + ', ' + date;
+            if(vm.weekNum == null) vm.weekNum = getWeekNum(y2k(now.getYear()),now.getMonth(),now.getDate());
 
             $scope.$on('post.created', function (event, post) {
                 console.log('post.created: scope get week' + post.weekNum);
@@ -49,7 +69,7 @@
                 vm.posts.shift();
             });
 
-            $scope.$on('post.getWeek', function (event, post){
+            $scope.$on('post.getWeek', function (event, post) {
                 console.log('scope get week' + post.weekNum);
                 Posts.getWeek(username, post.weekNum);
             });
@@ -78,13 +98,14 @@
              * @desc Update `posts` on viewmodel
              */
             function postsSuccessFn(data, status, headers, config) {
-                              console.log('post success: ');
+                console.log('post success: ');
                 vm.posts = data.data;
 
-                  var i;
-                  for(i = 0; i < vm.posts.length; i++){
-                      console.log(vm.posts[i].content);
-                  }
+                if(vm.posts[0].week_num != null) vm.weekNum = vm.posts[0].week_num;
+                var i;
+                for (i = 0; i < vm.posts.length; i++) {
+                    console.log(vm.posts[i].content);
+                }
 
             }
 
@@ -101,12 +122,30 @@
                 console.log('users success: ' + data.data);
                 vm.users = data.data;
 
-                  var i;
-                  for(i = 0; i < vm.users.length; i++){
-                      console.log(vm.users[i].username);
-                  }
+                var i;
+                for (i = 0; i < vm.users.length; i++) {
+                    console.log(vm.users[i].username);
+                }
 
             }
         }
     }
+
+    function getWeekNum(year, month, day) {
+        var when = new Date(year, month, day);
+        var newYear = new Date(year, 0, 1);
+        var offset = 7 + 1 - newYear.getDay();
+        if (offset == 8) offset = 1;
+        var daynum = ((Date.UTC(y2k(year), when.getMonth(), when.getDate(), 0, 0, 0) - Date.UTC(y2k(year), 0, 1, 0, 0, 0)) / 1000 / 60 / 60 / 24) + 1;
+        var weeknum = Math.floor((daynum - offset + 7) / 7);
+        if (weeknum == 0) {
+            year--;
+            var prevNewYear = new Date(year, 0, 1);
+            var prevOffset = 7 + 1 - prevNewYear.getDay();
+            if (prevOffset == 2 || prevOffset == 8) weeknum = 53; else weeknum = 52;
+        }
+        return weeknum;
+    }
+
+    function y2k(number) { return (number < 1000) ? number + 1900 : number; }
 })();
