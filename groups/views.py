23d16a70953e3e414f56dac7acd29c90692a253f
bitnami <ej48@duke.edu>
2015-02-15 00:18:29 +0000

    start groupviewset

--- groups/views.py
+++ groups/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from groups.models import KGroup
+from groups.serializers import GroupSerializer
 
-# Create your views here.
+class GroupViewSet(viewsets.ModelViewSet):
+    queryset = KGroup.objects.all()
+    serializer_class = GroupSerializer
+    
+    def perform_create(self, serializer):
+        print 'in perform create:'
+        instance = serializer.save(owner=self.request.user)
+
+        return super(GroupViewSet, self).perform_create(serializer)    
+
+
+class AccountGroupsViewSet(viewsets.ViewSet):
+    queryset = KGroup.objects.all()
+    serializer_class = GroupSerializer
+    
+    def list(self, request, account_username=None):
+        queryset = self.queryset.filter(owner__username=account_username)
+        serializer = self.serializer_class(queryset, many=True)
+        return Response(serializer.data)
+
+    
