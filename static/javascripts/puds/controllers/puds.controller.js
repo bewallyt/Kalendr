ea569ee533c3185fac7d65bf7cd6f97a2fe956a7
devdraweat <vmpritam@gmail.com>
2015-02-17 14:41:38 -0500

    completed activation code, render logic started

--- static/javascripts/puds/controllers/puds.controller.js
+++ static/javascripts/puds/controllers/puds.controller.js
@@ -16,6 +16,8 @@
      */
     function PudsController($timeout, $scope) {
         var vm = this;
+        vm.columns = [];
+
         activate();
 
 
@@ -26,6 +28,42 @@
          */
         function activate() {
 
+            var i;
+            for (i=0; i<$scope.puds.length; i++) {
+                console.log($scope.posts[i].content);
+            }
+            $scope.$watchCollection(function () {
+                return $scope.posts;
+            }, render);
+            $scope.$watch(function () {
+                return $(window).width();
+            }, render);
+        }
+
+        function calculateNumberOfColumns() {
+            var width = $(window).width();
+            return 3;
+        }
+
+        function render(current, original) {
+
+            if (current !== original) {
+                vm.columns = [];
+
+                for (var i=0; i<calculateNumberOfColumns(); ++i) {
+                    vm.columns.push([]);
+                }
+
+                //for (var i=0; i<current.length; ++i) {
+                //    if (current[i].repeat == 'Daily') {
+                //        vm.columns[0].push(current[i]);
+                //    } else if (current[i].repeat == 'Weekly') {
+                //        vm.columns[0].push(current[i]);
+                //    } else if (current[i].repeat == 'Monthly') {
+                //        vm.columns[1].push(current[i]);
+                //    }
+                //}
+            }
         }
 
     }
