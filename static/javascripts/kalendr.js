078aa8b1c6ab2b22ffa0352c0740c375bbe33331
devdraweat <vmpritam@gmail.com>
2015-03-30 23:32:49 -0400

    bower update, reincluded themes

--- static/javascripts/kalendr.js
+++ static/javascripts/kalendr.js
@@ -18,17 +18,16 @@
             'ui.bootstrap',
             'angucomplete',
             'ngMaterial'
-        ]);
-        //.config(function ($mdThemingProvider) {
-        //    $mdThemingProvider.theme('default')
-        //        .primaryPalette('blue', {
-        //            'default': '500', // by default use shade 400 from the blue palette for primary intentions
-        //            'hue-1': '100', // use shade 100 for the <code>md-hue-1</code> class
-        //            'hue-2': '600', // use shade 600 for the <code>md-hue-2</code> class
-        //            'hue-3': 'A100' // use shade A100 for the <code>md-hue-3</code> class
-        //        })
-        //        .accentPalette('orange');
-        //});
+        ]).config(function ($mdThemingProvider) {
+            $mdThemingProvider.theme('default')
+                .primaryPalette('blue', {
+                    'default': '500', // by default use shade 400 from the blue palette for primary intentions
+                    'hue-1': '100', // use shade 100 for the <code>md-hue-1</code> class
+                    'hue-2': '600', // use shade 600 for the <code>md-hue-2</code> class
+                    'hue-3': 'A100' // use shade A100 for the <code>md-hue-3</code> class
+                })
+                .accentPalette('orange');
+        });
 
     angular
         .module('kalendr.config', []);
